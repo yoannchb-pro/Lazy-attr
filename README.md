7286572d813b7c9494587c0ba1286cf4ae2aa871
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2020-12-17 15:21:06 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
-# 📖 Lazy-attr v1.0.4 - beta
+# 📖 Lazy-attr v1.0.5 - beta
 Create lazy image, embed, video... Make animation on lazy and not lazy element. All of that just with html attributes.
 ## 🎉 Update
 --------
 The project is in beta so don't worry for bugs
-- New attributes lazy-animation-pointer
+- New attributes : lazy-animation-pointer
 - New animations
 - New documentation
 - Bugs correction
 - Bugs animations correction
 - 2 Animations removed
 - Video bugs correction
+- Video animation correction
+- Image loading animation correction
+- Handle error
 - min files added
 ## 📚 Documentation
 --------
@@ -21,8 +24,8 @@ The project is in beta so don't worry for bugs
 <script src="./lazy-attr.min.js"></script>
 <link src="./lazy-attr-animation.min.css"/>
 <!-- or -->
-<script src="https://cdn.jsdelivr.net/npm/lazy-attr@1.0.3/lazy-attr.min.js"></script>
-<link src="https://cdn.jsdelivr.net/npm/lazy-attr@1.0.3/lazy-attr-animation.min.css"/>
+<script src="https://cdn.jsdelivr.net/npm/lazy-attr@1.0.5/lazy-attr.min.js"></script>
+<link src="https://cdn.jsdelivr.net/npm/lazy-attr@1.0.5/lazy-attr-animation.min.css"/>
 ```
 ## 🐱 Github and NPM
 --------
--- lazy-attr.js
+++ lazy-attr.js
@@ -1,6 +1,6 @@
 /**
  * https://github.com/yoannchb-pro/Lazy-attr
- * VERSION: 1.0.4
+ * VERSION: 1.0.5
  */
 
 window.lazy = () => {
@@ -34,12 +34,16 @@ window.lazy = () => {
             threshold: 0
         },
         //version
-        version: "1.0.4"
+        version: "1.0.5"
     }
 }
 
 //DOM loaded
 window.addEventListener('load', () => {
+    //Function to display error and info
+    const displayInfo = m => console.log(`[INFO] Lazy-attr : ${m}`);
+    const displayError = m => console.error(`[ERROR] Lazy-attr : ${m}`);
+
     let callback = (entries, observer) => {
         entries.forEach(entry => {
             const target = entry.target; //Element target
@@ -53,26 +57,54 @@ window.addEventListener('load', () => {
                 let animationDelay = target.getAttribute('lazy-animation-delay');
                 if(animationDelay) target.style.animationDelay = `${animationDelay}s`;
 
-                const startAnimation = () => {
-                    //Animation class
-                    let animationClass = target.getAttribute('lazy-animation');
-                    let pointer = target.getAttribute('lazy-animation-pointer');
-                    if(pointer && animationClass){
-                        let t = document.querySelectorAll(pointer);
-                        t.forEach(e => {
-                            e.classList.add(animationClass);
-                        });
-                    } else {
-                        if(animationClass) target.classList.add(animationClass);
-                    }
 
-                    if(target.getAttribute('lazy-reset') === null){ //Used to reload animation on scroll
+                //Run or pause animation function
+                const animationState = (e, m) => {
+                    e.style.animationPlayState = m;
+                    e.webkitAnimationPlayState = m;
+                };
+
+                //Animation class
+                let animationClass = target.getAttribute('lazy-animation');
+                let pointer = target.getAttribute('lazy-animation-pointer');
+                const targetAnimation = [];
+                if(pointer && animationClass){
+                    let t = document.querySelectorAll(pointer);
+                    t.forEach(e => {
+                        e.classList.add(animationClass);
+                        animationState(e, "paused");
+                        targetAnimation.push(e);
+                    });
+                } else {
+                    if(animationClass){
+                        target.classList.add(animationClass);
+                        animationState(target, "paused");
+                        targetAnimation.push(target);
+                    }
+                }
+
+                //Start animation function after load element if it is lazy or not
+                //document.querySelector('img').style.animationPlayState .webkitAnimationPlayState
+                const startAnimation = () => {
+                    targetAnimation.forEach(e => {
+                        animationState(e, "running");
+                    });
+
+                    if(target.getAttribute('lazy-reset') === null){ //Used to don't reload animation on scroll
                         target.removeAttribute('lazy-animation');
                     }
                 }
-                if(target.complete || !target.getAttribute("lazy-src")) startAnimation(); else {
+
+                let loadedImg = target.complete && target.naturalHeight !== 0;
+                let loadedVideo = target.readyState >=0; //>=3
+                let loaded = loadedImg || loadedVideo;
+                if(loaded || !target.getAttribute("lazy-src")) startAnimation(); else {
+                    target.addEventListener('error', () => {
+                        startAnimation();
+                        displayError("cannot load url " + target.src);
+                    });
                     target.addEventListener('load', startAnimation);
-                    target.addEventListener('loadeddata', startAnimation);
+                    //target.addEventListener('loadeddata', startAnimation);
                 }
 
                 //Lazy video
@@ -123,20 +155,24 @@ window.addEventListener('load', () => {
         });
     }
 
-    //Observer
-    let observer = new IntersectionObserver(callback, window.lazy().options);
+    //Get available
+    if(window.IntersectionObserver && window.lazy()){
+        //Observer
+        let observer = new IntersectionObserver(callback, window.lazy().options);
 
-    //Set observer on dom elements
-    const getLazyObject = () => {
-        document.body.querySelectorAll(window.lazy().parameters.join(',')).forEach(e => {
-            observer.observe(e);
-        });
+        //Set observer on dom elements
+        const getLazyObject = () => {
+            document.body.querySelectorAll(window.lazy().parameters.join(',')).forEach(e => {
+                observer.observe(e);
+            });
+        }
+        document.addEventListener("DOMNodeInserted", getLazyObject);
+        getLazyObject();
+
+        //Info
+        displayInfo('version ' + window.lazy().version);
+    } else {
+        //Error
+        displayError('incompatible or verify window.lazy integration');
     }
-    document.addEventListener("DOMNodeInserted", getLazyObject);
-    getLazyObject();
-
-    //Display info
-    const displayInfo = m => console.log(`[INFO] Lazy - ${m}`);
-    displayInfo('configuration have been lauch');
-    displayInfo('version ' + window.lazy().version);
 });
